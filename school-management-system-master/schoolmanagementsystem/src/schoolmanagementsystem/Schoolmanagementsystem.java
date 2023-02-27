da0d1889e1336f2e4c63309afd7009b6b18eff19
Charan <124618186+Charan201002@users.noreply.github.com>
2023-02-27 14:32:26 +0530

    Add files via upload

---
+++ school-management-system-master/schoolmanagementsystem/src/schoolmanagementsystem/Schoolmanagementsystem.java
@@ -0,0 +1,17 @@
+
+package schoolmanagementsystem;
+
+/**
+ *
+ * @author dell
+ */
+public class Schoolmanagementsystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
